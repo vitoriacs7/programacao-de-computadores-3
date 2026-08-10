21644058a10d5089ff05eccffd22ecc87b772a5e
Vitória <bandeiravitoria066@gmail.com>
2026-08-10 09:15:03 -0300

    Add tipovar.js to demonstrate variable types

---
+++ tipovar.js
@@ -0,0 +1,11 @@
+var numero = 5.2;
+console.log(typeof(numero));
+
+var palavra = "JavaScript";
+console.log(typeof(palavra));
+
+var logica = false;
+console.log(typeof(logica));
+
+var qualquer;
+console.log(typeof(qualquer));
