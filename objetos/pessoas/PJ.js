07c643f542df8c2b429bfa6a56da0dba3fbe6b01
null <null>
2026-09-14 11:16:03 -0300

    OO

---
+++ objetos/pessoas/PJ.js
@@ -0,0 +1,47 @@
+const Pessoa = require('./Pessoa');
+
+class PJ extends Pessoa {
+
+    #cnpj;
+
+    setCNPJ(cnpj) {
+
+     if (cnpj) {
+
+        if (cnpj.lenght < 18) {
+            return false;
+        }
+
+        this.#cnpj = cnpj;
+        return true;
+
+        } else {
+
+        return false;
+
+        }
+    }
+
+    getCNPJ() {
+        return this.#cnpj;
+    }
+
+    setEmail(email) { // Sobrescrita do método setEmail()
+
+        if (email !== '') {
+
+            if (email.includes('@')) {
+
+                super.setEmail(email);
+
+                return true;
+
+            }
+        } else {
+
+            return false;
+        }
+    }
+}
+
+module.exports = PJ;
--- objetos/pessoas/usaPessoas.js
+++ objetos/pessoas/usaPessoas.js
@@ -1,10 +1,12 @@
+console.log("==== Objeto Pessoa ====")
+
 const Pessoa = require('./pessoas/Pessoa');
-const PF = require('./pessoas/PF');
 
 const x = new Pessoa();
 
-let reposta = x.setNome('');
+reposta = x.setNome('');
 console.log(resposta);
+
 if (resposta) {
     console.log(x.getNome());
 } else {
@@ -13,21 +15,31 @@ if (resposta) {
 
 resposta = x.setEmail('vitoria@gmail.com');
 console.log(reposta);
+
 if (resposta) {
     console.log(x.getEmail());
 } else {
     console.log("E-mail vazio!");
 }
 
+console.log("==== Objeto PF ====")
+
+const PF = require('./pessoas/PF');
+
 const y = new PF ();
+
 resposta = y.setNome('Vitória');
 console.log(resposta);
+
 if (resposta) {
     console.log(y.getNome());
+} else {
+    console.log("Nome vazio");
 }
 
 resposta = y.setEmail('');
 console.log(resposta);
+
 if (resposta) {
     console.log(y.getEmail());
 } else {
@@ -36,6 +48,33 @@ if (resposta) {
 
 resposta = y.setCPF('123456789-10');
 console.log(resposta);
+
 if (resposta) {
     console.log(y.getCPF());
+} else {
+    console.log("CPF vazio");
+}
+
+console.log("==== Objeto PF ====");
+
+const PJ = require('./pessoas/PJ');
+
+const z = new PJ();
+
+resposta = z.setNome('Empresa ABC');
+console.log(resposta);
+
+if (resposta) {
+    console.log(z.getEmail());
+} else {
+    console.log("E-mail inválido");
+}
+
+resposta = z.setCNPJ('12.345.678/0001-90');
+console.log(resposta);
+
+if (resposta) {
+    console.log(z.getCNPJ());
+} else {
+    console.log("CNPJ inválido");
 }
